ec779851b71c54341fed9f3da3d57c7964c8de11
luoye <bookzhan@qq.com>
2023-04-25 21:42:15 +0800

    init commit

---
+++ app/src/main/java/com/example/ffmpegtest/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.ffmpegtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.ffmpegtest.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'ffmpegtest' library on application startup.
+    static {
+        System.loadLibrary("ffmpegtest");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+    }
+
+    /**
+     * A native method that is implemented by the 'ffmpegtest' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    public void test(View view) {
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+}
